8c671ec4144b61e0129faa09fe22055770c16a98
Przemysław Gawron <przemyslaw.gawron@propoint.pl>
2021-02-25 19:10:43 +0100

    change bootstrap file

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './bootstrap2.min.css'
+import './bootstrap.min.css'
 import { AppProviders } from './providers/AppProviders'
 import { App } from './app/App'
 import './index.css'
